82db1626066b9d0e223777cd2b1ba85e7a0ce0be
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-10-19 16:01:38 +0500

    Swagger added to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const start = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('EDU-erp')
+      .setDescription('Program for automation of educational centers.')
+      .addTag('NodeJs NestJs Postgresql, Prisma JWT Swagger')
+      .setVersion('1.0.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, document);
+
+    const PORT = process.env.PORT || 3003;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error, 'path: main.ts');
+  }
+};
+start();
